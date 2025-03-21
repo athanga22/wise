0bef3afb2811fa9bbfba8d4ea05c3c3d28879d26
athanga22 <158106630+athanga22@users.noreply.github.com>
2025-03-20 18:39:44 -0700

    Register component refactoring

--- frontend/src/services/users/userServices.js
+++ frontend/src/services/users/userServices.js
@@ -1,11 +1,24 @@
 import axios from "axios";
 import { BASE_URL } from "../../utils/url";
 
-//lOGIN
+//LOGIN
 export const loginAPI = async({email, password}) => {
     const response = await axios.post(`${BASE_URL}/users/login`, {
         email,
         password,
+    });
+    
+    //return a promise
+    return response.data;
+};
+
+//REGISTER
+export const registerAPI = async({username, email, password, confirmPassword}) => {
+    const response = await axios.post(`${BASE_URL}/users/register`, {
+        name: username,
+        email,
+        password,
+        confirmPassword
     });
     
     //return a promise
